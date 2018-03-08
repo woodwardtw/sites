3426a0f4ddeba861fa1e5525a7cd422b587d0fcd
Tom Woodward <twwoodward@me.com>
2018-03-08 13:52:38 -0500

    addition of display options

---
+++ js/sites.js
@@ -0,0 +1,37 @@
+jQuery( document ).ready(function() {
+	var divs = ['child-posts','child-pages'];
+	divs.forEach(function(element) {
+		if (document.getElementById(element)){
+	    	var postsDiv = document.getElementById(element);
+	    	console.log(postsDiv.dataset.url);
+	    	var url = postsDiv.dataset.url;
+	    	makeContent(url, element);
+	    }
+	})	
+});
+
+
+function makeContent (url, destination){
+	fetch(url)
+	  .then(function(response) {
+	  // Convert to JSON
+	  return response.json();
+	})
+	  .then(function(data) {
+	  // GOOD!
+	  	  for (i = 0; i < data.length; i++) {
+	    writeEvents(data[i], destination);
+	  } 
+	});
+
+function writeEvents(data, destination) {
+  var targetDiv = '#'+destination;
+  console.log(targetDiv);	
+  var post = jQuery(targetDiv).append(
+    jQuery(
+       '<div class="child-post-content"><a href="' + data.link + '"><h4>' + data.title.rendered + '</h4></a></div>'
+    ) 
+  );
+}
+
+}
--- site.php
+++ site.php
@@ -4,25 +4,37 @@
     	<div class="timeline">
 			<div class="content-area">
 				<main id="main" class="site-main" role="main">
-       				 <div id="timeline-embed"></div>
+       				 <div id="content">
 				        <?php if(have_posts()): while(have_posts()): the_post(); ?>
 				           <?php 
 				          	 $post_id = get_the_ID();
 				          	 $url = get_post_meta($post_id, 'site-url', true);
-				          	echo '<h2>post_id' . $post_id . '</h2>';
-				          	echo '<h3>'.the_title().'</h3>';
+				          	 $name = get_post_meta($post_id, 'child-title', true);
+				          	 $description = get_post_meta($post_id, 'child-description', true);
 
-				          	//test
-				          	$url = get_post_meta($post_id, 'site-url', true);
-				          	var_dump($url);
-							 var_dump(get_title($url['text']));
+				          	$posts = get_post_meta($post_id, 'total-posts', true);
+							$pages = get_post_meta($post_id, 'total-pages', true);
 
+							$posts_url = get_post_meta($post_id, 'full-api-url', true) . 'wp/v2/posts?per_page=10';
+							$pages_url = get_post_meta($post_id, 'full-api-url', true) . 'wp/v2/pages?per_page=10';
 
+				          	echo '<h1 class="site-title">' . $name . '</h1>';
+				          	if($description){
+				          		echo '<h2>'. $description .'</h2>';
+				          	} 
+				          	if ($posts > 0){
+				          		echo '<h3 class="child-content-header"> Posts: ' . $posts . '</h3>';
+				          		echo '<div class="child-content" id="child-posts" data-url="' . $posts_url . '"></div>';
+				          	}
+				          	if ($pages > 0 ){
+				          		echo '<h3 class="child-content-header">Pages: ' . $pages . '</h3>';
+				          		echo '<div class="child-content" id="child-pages" data-url="' . $pages_url . '"></div>';				          	
+				          	}
+				          	
 				          	 ?>	          	 		
 						<?php endwhile; endif;?>
 
-				        
-
+				    </div>      
 				</main><!-- #main -->
 			</div><!-- .content-area -->
 		</div><!-- .timeline -->
